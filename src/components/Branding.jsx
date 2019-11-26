bd9cba0dbc203254dca44150d996670c0bef054c
Martin Panin <ysda666@gmail.com>
2019-11-26 23:46:50 +0200

    init

--- src/components/Branding.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const Branding = () => (
-    <footer className="branding">
-        <span>Developed by <br/><a target="_blank" rel="noopener noreferrer" href="https://geekdev.ee/">GeekDev.ee</a></span>
-    </footer>
-);
-export default Branding;
