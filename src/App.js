0f288bfcb82921caee08eef39fc61ddb8a08798e
Martin Panin <ysda666@gmail.com>
2019-11-27 08:40:42 +0200

    diplom

--- src/App.js
+++ src/App.js
@@ -31,6 +31,20 @@ class App extends Component {
           })
         }
 
+
+
+        handlePrint=()=>{
+            this.setState({
+                hide:true
+            })
+            window.scroll(0,0);
+
+        }
+
+        actionPrint=()=>{
+            window.print();
+        }
+
         handleBg=()=>{
           const body = document.querySelector('#root')
           
@@ -62,44 +76,48 @@ class App extends Component {
   render({hide, edit, title,subTitle,name,place,date,info,category, category2} = this.state) {
     return (
       <React.Fragment>
-        <section className={hide? `hidden absolute basis100` : 'absolute basis100'} style={{top:0, left:0}}>
-          <button onClick={this.handleHide} className={hide? `hidden` : ''}> Hide</button>
-          <button onClick={this.handleBg} className={hide? `hidden` : ''}> BG</button>
-          <button onClick={this.handleEdit} className={hide? `hidden` : ''}> Edit</button>
+        <section className={hide? `hide` : 'fixed top left'} style={{top:0, left:0}}>
+          <button onClick={this.handleHide} className={hide? `hide` : ''}> Hide</button>
+          <button onClick={this.handleBg} className={hide? `hide` : ''}> BG</button>
+          <button onClick={this.handleEdit} className={hide? `hide` : ''}> Edit</button>
+            <button onClick={this.handlePrint} className={hide? `hide` : ''}> Print</button>
         </section>
         <section className={`container gridX`}>
         
           <section className={`logo gridX basis100 justifyCenter textCenter`}>
-            <img src="/img/logo.svg"/>
+            <img onClick={this.handleHide} src="/img/logo.svg" alt={`EVK logo`} />
           </section>
           <section className={`gridX basis100 justifyCenter uppercase`}>
-            {edit? <input type={`text`} onChange={this.handleChange} name={`title`} value={title}/> : 
-            <h1 className={`basis100 justifyCenter alignCenter`}>{title}</h1>}
-            {edit? <input type={`text`} onChange={this.handleChange} name={`subTitle`} value={subTitle}/> : 
-            <h2 className={`basis100 justifyCenter alignCenter`}>{subTitle}</h2>}
+            {edit?
+                <input className={`basis100 line justifyCenter alignLeft textleft`} type={`text`} onChange={this.handleChange} name={`title`} value={title}/> :
+                <h1 className={`basis100 justifyCenter alignCenter`}>{title}</h1>}
+            {edit?
+                <input className={`basis100 line justifyCenter alignLeft textleft`} type={`text`} onChange={this.handleChange} name={`subTitle`} value={subTitle}/> :
+            <h2 className={`basis75 justifyCenter alignCenter`}>{subTitle}</h2>}
           </section>
           <section className={`gridX basis100 justifyCenter uppercase`}>
-          {edit? <input type={`text`} onChange={this.handleChange} name={`name`} value={name}/> : 
-          <h3 className={`basis100 line justifyCenter alignLeft textleft`}>{name}</h3>}
-          {edit? <input type={`text`} onChange={this.handleChange} name={`place`} value={place}/> : 
-           <h3 className={`basis100 line justifyCenter alignLeft textleft`}>{place}</h3>}
-            
-           
+              {edit?
+                  <input className={`basis100 line justifyCenter alignLeft textleft`} type={`text`} onChange={this.handleChange} name={`name`} value={name}/> :
+              <h3 className={`basis100 line justifyCenter alignLeft textleft`}>{name}</h3>}
+              {edit?
+                  <input className={`basis100 line justifyCenter alignLeft textleft`} type={`text`} onChange={this.handleChange} name={`place`} value={place}/> :
+               <h3 className={`basis100 line justifyCenter alignLeft textleft`}>{place}</h3>}
           </section>
           <section className={`gridX basis100 justifyCenter alignLeft`}>
-          {edit? <input type={`text`} onChange={this.handleChange} name={`category`} value={category}/>
-         : 
-          <h3 className={`basis100 justifyCenter m-0 alignCenter`}>{category}<br/>{category2}</h3>}
-           {edit? <input type={`text`} onChange={this.handleChange} name={`category2`} value={category2}/>:''}
-  
+              {edit?
+                  <input className={`basis100 line justifyCenter alignLeft textleft`} type={`text`} onChange={this.handleChange} name={`category`} value={category}/>
+             :
+              <h3 className={`basis100 justifyCenter m-0 alignCenter`}>{category}<br/>{category2}</h3>}
+               {edit? <input className={`basis100 line justifyCenter alignLeft textleft`} type={`text`} onChange={this.handleChange} name={`category2`} value={category2}/>:''}
+
           </section>
-          <section className={`gridX basis100 justifyCenter alignRight`}>
-          {edit? <input type={`text`} onChange={this.handleChange} name={`date`} value={date}/> :  
-          <h4 className={`basis50 justifyCenter alignLeft textleft uppercase`}>{date}</h4>}
-          {edit? <input type={`text`} onChange={this.handleChange} name={`info`} value={info}/> : 
-          <h4 className={`basis50 justifyCenter alignRight textRight`}>{info}</h4>}
-           
-            
+          <section className={`gridX basis100 justifySpaced alignRight`}>
+              {edit?
+                  <input className={`basis100 line justifyCenter alignLeft textleft`} type={`text`} onChange={this.handleChange} name={`date`} value={date}/> :
+              <h4 className={`basis50 justifyCenter alignLeft textleft uppercase`}>{date}</h4>}
+              {edit?
+                  <input className={`basis100 line justifyCenter alignLeft textleft`} type={`text`} onChange={this.handleChange} name={`info`} value={info}/> :
+              <h4 className={`basis25 justifyCenter alignRight textRight`}>{info}</h4>}
           </section>
         </section>
 
